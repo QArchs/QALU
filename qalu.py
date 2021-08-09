6edc70755a451fe3e5737d8e036dad83c0b58c36
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2021-08-10 00:14:25 +0300

    Update qalu.py

--- qalu.py
+++ qalu.py
@@ -23,3 +23,28 @@ def nor_(circ,a,b,out):
 def xnor_(circ,a,b,out):
     circ.cx(a,out)
     circ.cx(b,out)
+
+def half_adder(circ, a, b, carry,sum_):
+    xor_(circ,a,b,sum_)
+    and_(circ,a,b,carry)
+    
+def full_adder(circ,a,b,c_in,t_0,t_1,t_2,c_out,sum_):
+    xor_(circ,a,b,t_0)
+    xor_(circ,t_0,c_in,sum_)
+    and_(circ,a,b,t_1)
+    and_(circ,c_in,t_0,t_2)
+    or_(circ,t_1,t_2,c_out)
+    
+def half_subtractor(circ, a, b, carry, diff):
+    xor_(circ,a,b,diff)
+    circ.ccx(b,diff,carry)
+    
+def full_subtractor(circ,a,b,c_in,t_0,t_1,t_2,c_out,diff):
+    xor_(circ,a,b,t_0)
+    xor_(circ,t_0,c_in,diff)
+    circ.x(a)
+    circ.ccx(a,b,t_1)
+    circ.x(a)
+    circ.x(t_0)
+    circ.ccx(t_0,c_in,t_2)
+    or_(circ,t_1,t_2,c_out)
