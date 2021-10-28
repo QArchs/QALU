ecf58c75825db06c12d2630dd6187ca3dacb64d8
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2021-10-28 17:23:43 +0300

    Update qalu.py

--- qalu.py
+++ qalu.py
@@ -29,26 +29,26 @@ def half_adder(circ, a, b, carry,sum_):
     and_(circ,a,b,carry)
     
 def full_adder(circ,a,b,c_in,c_out,sum_):
-    qc.cx(a,sum_)
-    qc.cx(b,sum_)
-    qc.cx(c_in,sum_)
-    qc.ccx(a,b,c_out)
-    qc.ccx(b,c_in,c_out)
-    qc.ccx(c_in,a,c_out)
+    circ.cx(a,sum_)
+    circ.cx(b,sum_)
+    circ.cx(c_in,sum_)
+    circ.ccx(a,b,c_out)
+    circ.ccx(b,c_in,c_out)
+    circ.ccx(c_in,a,c_out)
     
 def half_subtractor(circ, a, b, carry, diff):
     xor_(circ,a,b,diff)
     circ.ccx(b,diff,carry)
     
 def full_subtractor(circ,a,b,b_in,b_out,diff):
-    qc.cx(a,diff)
-    qc.cx(b,diff)
-    qc.cx(b_in,diff)
-    qc.x(a)
-    qc.ccx(b_in,a,b_out)
-    qc.ccx(b_in,b,b_out)
-    qc.ccx(a,b,b_out)
-    qc.x(a)
+    circ.cx(a,diff)
+    circ.cx(b,diff)
+    circ.cx(b_in,diff)
+    circ.x(a)
+    circ.ccx(b_in,a,b_out)
+    circ.ccx(b_in,b,b_out)
+    circ.ccx(a,b,b_out)
+    circ.x(a)
   
 def multi_qubits_adder(circ,A,B,T,C):
     if not (type(A)==tuple or type(A)==list):
